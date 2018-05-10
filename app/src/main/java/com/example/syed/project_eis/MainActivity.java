5a6bd37478ac6bd47e69d825fc6d9ee3c2348bc8
Syed Jahangir <syedjahangir.p16@iiits.in>
2018-05-10 19:50:56 +0530

    first commit

---
+++ app/src/main/java/com/example/syed/project_eis/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.syed.project_eis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    public WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView = findViewById(R.id.w1);
+        WebSettings webSettings = mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("http://ajitjadhav.pythonanywhere.com/pollution-data/showdata/");
+        mywebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack()){
+            mywebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
